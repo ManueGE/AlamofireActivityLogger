6f8ef5d0c2f47c2660da0d8827e23fb27b7e5c40
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-05-30 16:27:55 +0200

    add logic to sample app

--- alamofire_activity_logger/ViewController.swift
+++ alamofire_activity_logger/ViewController.swift
@@ -13,17 +13,69 @@ let successURL = "http://www.mocky.io/v2/574c43dc100000760eed69ca"
 let failURL = "http://www.mocky.io/v2/574c440d100000860eed69cb"
 
 class ViewController: UIViewController {
-
+    
+    let levels: [(LogLevel, String)] = [
+        (.None, "None"),
+        (.All, "All"),
+        (.Info, "Info"),
+        (.Error, "Error")
+    ]
+    
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var prettyPrintSwitch: UISwitch!
+    @IBOutlet var includeSeparatorSwift: UISwitch!
+    
+    @IBOutlet var views: [UIControl]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        segmentedControl.removeAllSegments()
+        levels.enumerate().forEach { (index, element) in
+            segmentedControl.insertSegmentWithTitle(element.1,
+                atIndex: index,
+                animated: false)
+        }
+        segmentedControl.selectedSegmentIndex = 1
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: Actions
+    @IBAction func didPressSuccess(sender: AnyObject) {
+        performRequest(withURL: successURL)
     }
-
-
+    
+    @IBAction func didPressError(sender: AnyObject) {
+        performRequest(withURL: failURL)
+    }
+    
+    private func performRequest(withURL URL: String) {
+        
+        // Build options
+        var options: [LogOptions] = []
+        
+        if prettyPrintSwitch.on {
+            options.append(.JSONPrettyPrint)
+        }
+        
+        if includeSeparatorSwift.on {
+            options.append(.IncludeSeparator)
+        }
+        
+        // Level
+        let level = levels[segmentedControl.selectedSegmentIndex].0
+        
+        self.setViewEnabled(false)
+        request(.GET, URL)
+            .validate()
+            .debugLog(level, options: options)
+            .responseData { (response) in
+                self.setViewEnabled(true)
+        }
+    }
+    
+    // MARK: Helpers
+    func setViewEnabled(enabled: Bool) {
+        views.forEach( { $0.enabled = enabled } )
+    }
 }
 
